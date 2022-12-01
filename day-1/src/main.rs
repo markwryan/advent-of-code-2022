0885cc690d1848dc48039dcd0e45b2f0e87b31e8
Mark Ryan <mark.ryan@getordermark.com>
2022-12-01 13:13:10 -0500

    Ran cargo fmt

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,16 +1,14 @@
 use std::fs;
 
 fn main() {
-    let content = fs::read_to_string("input.txt")
-        .expect("Should have been able to read the file");
+    let content = fs::read_to_string("input.txt").expect("Should have been able to read the file");
     let lines = content.lines();
     let mut cal_count = 0;
     let mut elves = Vec::<i32>::new();
     for i in lines {
         if i != "" {
             cal_count += i.parse::<i32>().unwrap();
-        }
-        else {
+        } else {
             elves.push(cal_count);
             cal_count = 0;
         }
@@ -20,5 +18,5 @@ fn main() {
     // Part 1
     println!("Part 1: {}", elves[0]);
     // Part 2
-    println!("Part 2: {}", elves[0]+elves[1]+elves[2]);
+    println!("Part 2: {}", elves[0] + elves[1] + elves[2]);
 }
