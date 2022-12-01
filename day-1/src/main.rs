1c4159ee8d574b8186282278a35c3227bfed54d7
Mark Ryan <mark.ryan@getordermark.com>
2022-12-01 12:53:51 -0500

    Fix some bad changes via last cleanup commit. Completed part 2.

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,20 +1,24 @@
 use std::fs;
 
 fn main() {
-    let lines = fs::read_to_string("input.txt").lines()
+    let content = fs::read_to_string("input.txt")
         .expect("Should have been able to read the file");
+    let lines = content.lines();
     let mut cal_count = 0;
-    let mut cal_max = 0;
+    let mut elves = Vec::<i32>::new();
     for i in lines {
         if i != "" {
             cal_count += i.parse::<i32>().unwrap();
         }
         else {
-            if cal_count > cal_max{
-                cal_max = cal_count;
-            }
+            elves.push(cal_count);
             cal_count = 0;
         }
     }
-    println!("{}", cal_max);
+    elves.sort();
+    elves.reverse();
+    // Part 1
+    println!("Part 1: {}", elves[0]);
+    // Part 2
+    println!("Part 2: {}", elves[0]+elves[1]+elves[2]);
 }
