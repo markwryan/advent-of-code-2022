2ae3a66a726ff0ffc3960e857995b6548a7aaa4d
Mark Ryan <mark.ryan@getordermark.com>
2022-12-09 11:59:48 -0500

    Working Day 7, Part 1

--- day-7/src/main.rs
+++ day-7/src/main.rs
@@ -1,36 +1,27 @@
 use std::fs;
 use std::collections::HashMap;
 
-#[derive(Debug, Clone)]
-struct Directory {
-    files: Vec<File>,
-    parent: String,
-}
-
-#[derive(Debug, Clone)]
-struct File {
-    name: String,
-    size: i32,
-}
-
 fn main() {
     let content = fs::read_to_string("input.txt").expect("Should have been able to read the file");
     let lines = content.lines();
-    let mut directories: HashMap<String, Directory> = HashMap::new();
-    let mut current_directory_name = "/"; 
+    let mut pwd: Vec<String> = vec!();
+    let mut filesystem: HashMap<String, usize> = HashMap::new();
     for line in lines {
         let input: Vec<_> = line.split_whitespace().collect();
-        let current_directory = directories.get(current_directory_name);
+        // Changing into child directory
         if input[0] == "$" && input[1] == "cd" && input[2] != ".." {
-            let dir = Directory {
-                files: vec!(),
-                parent: (&current_directory_name).to_string(),
-            };
-            directories.insert(input[2].to_string(), dir);
-            current_directory_name = input[2];
+            if input[2] != "/" {
+                let directory_name = pwd.clone().join("") + "/" + &input[2].to_string();
+                let present_directory = directory_name.clone();
+                filesystem.insert(directory_name, 0);
+                pwd.push(present_directory);
+            }
+            else {
+            }
+                   
         }
         else if input[0] == "$" && input[1] == "cd" && input[2] == ".." {
-            current_directory_name = &current_directory.unwrap().parent;
+            pwd.pop();
         }
         else if input[0] == "$" && input[1] == "ls" {
             // dont care
@@ -39,36 +30,22 @@ fn main() {
             //dont care
         }
         else {
-            let f = File{name: input[1].to_string(), size: input[0].parse().unwrap()};
-            let dir = current_directory.unwrap();
-
-            let mut up = Directory {
-                files: dir.files.to_vec(),
-                parent: dir.parent.to_string(),
-            };
-            up.files.push(f);
-            directories.insert(current_directory_name.to_string(), up);
+            let file_size: usize = input[0].parse().unwrap();
+            let update_paths = pwd.clone();
+            for v in update_paths {
+                let mut size = filesystem.get(&v).expect("Directory should exist.").clone();
+                size = size + file_size;
+                filesystem.insert(v, size);
+            }
         }
     }
-
-    for (k, _v) in &directories {
-        let s = calculate_size(&k, &directories);
-        if s <= 100000 {
-            println!("{}", k.to_string());
-        }
-    }
-}
-
-fn calculate_size(directory_name: &String , directories: &HashMap<String, Directory>) -> i32 {
     let mut total = 0;
-    let d = directories.get(directory_name).expect("directory should exist");
-    for file in d.files.iter() {
-        total = total + file.size;
+    for (k,v) in filesystem {
+        if v <= 100000 {
+            total = total + v;
+        }
     }
-    if d.parent != "_" {
-        total = total + calculate_size(&d.parent, &directories);
-    }
-    
-    return total;
-}
+    println!("Part 1: {total}");
 
+
+}
