5e1688b095d70e1ba7a69b32f2781d0ab5cb5e28
Mark Ryan <mark@markryan.email>
2022-12-16 16:12:48 -0500

    Working solutions for part 8

--- day-8/src/main.rs
+++ day-8/src/main.rs
@@ -1,9 +1,13 @@
 use std::fs;
 
 fn main() {
+    const WIDTH: usize = 99;
+    const HEIGHT: usize = 99;
     let content = fs::read_to_string("input.txt").expect("Should have been able to read the file");
-    let mut tree_map:[[u32;100];99] = [[0;100];99];
+    let mut tree_map: [[u32; WIDTH]; HEIGHT] = [[0; WIDTH]; HEIGHT];
     let mut row = 0;
+    let mut scenic_score: Vec<i32> = vec![];
+
     for line in content.lines() {
         let mut column = 0;
         for tree in line.chars() {
@@ -13,11 +17,63 @@ fn main() {
         row += 1;
     }
 
-    for r in 1..98 {
-        for c in 1..99 {
+    let mut visible = 0;
+    for r in 1..HEIGHT - 1 {
+        for c in 1..WIDTH - 1 {
             let t = tree_map[r][c];
-            print!("{t}");
+            let mut north = true;
+            let mut north_count = 0;
+            let mut n: i32 = r as i32 - 1;
+            while n >= 0 {
+                north_count += 1;
+                if t <= tree_map[n as usize][c] {
+                    north = false;
+                    break;
+                }
+                n -= 1;
+            }
+
+            let mut south = true;
+            let mut south_count = 0;
+            for s in r + 1..HEIGHT {
+                south_count += 1;
+                if t <= tree_map[s][c] {
+                    south = false;
+                    break;
+                }
+            }
+
+            let mut west = true;
+            let mut west_count = 0;
+            let mut w = c as i32 - 1;
+            while w >= 0 {
+                west_count += 1;
+                if t <= tree_map[r][w as usize] {
+                    west = false;
+                    break;
+                }
+                w -= 1;
+            }
+
+            let mut east = true;
+            let mut east_count = 0;
+            for e in c + 1..WIDTH {
+                east_count += 1;
+                if t <= tree_map[r][e] {
+                    east = false;
+                    break;
+                }
+            }
+
+            if north || south || east || west {
+                visible += 1;
+            }
+            let scenic = north_count * south_count * east_count * west_count;
+            scenic_score.push(scenic);
         }
-        println!("");
     }
+    visible += 99 + 99 + 97 + 97;
+    println!("Part 1 {visible}");
+    scenic_score.sort();
+    println!("Part 2 {}", scenic_score.last().expect("vector not empty"));
 }
